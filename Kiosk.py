378eac1293f1d2fc3c0f25b722c89054a07d1614
nhaun24 <61599705+nhaun24@users.noreply.github.com>
2023-05-18 21:19:08 -0400

    Security update

--- Kiosk.py
+++ Kiosk.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.options import Options
+import configparser
 
 # Path to the edgedriver executable
 driver_path = '/path/to/edgedriver'
@@ -13,14 +14,24 @@ driver_path = '/path/to/edgedriver'
 # Create Edge options
 edge_options = Options()
 edge_options.use_chromium = True
-edge_options.add_argument("--start-maximized")  # Add this argument to start the browser in full-screen mode
+edge_options.add_argument("--kiosk")  # Add this argument to start the browser in full-screen mode
 
 # URL of the webpage you want to open
 url = 'https://onrealm.org/fbcathens/Home/Tasks?redirectController=Individual&redirectAction=Info&redirectId=b1639cf8-8fc1-4287-a213-ad7a0148dd6a'
 
-# Embedded credentials
-username = 'admin@fbcathens.com'
-password = 'Kiosk1218'
+# Read the credentials from the configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+username = config.get('Credentials', 'username')
+password = config.get('Credentials', 'password')
+
+# If the credentials are not found or empty, prompt the user to enter them interactively
+if not username:
+    username = input('Enter your username: ')
+
+if not password:
+    password = getpass.getpass('Enter your password: ')
+
 
 # Launch Microsoft Edge browser using edgedriver
 driver = webdriver.Edge(executable_path=driver_path, options=edge_options)
