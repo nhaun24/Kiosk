3e9f2aa53bf886a1b7a66f499c52a0496518bdb8
nhaun24 <61599705+nhaun24@users.noreply.github.com>
2023-12-13 14:11:13 -0500

    Update Kiosk.py

--- Kiosk.py
+++ Kiosk.py
@@ -45,7 +45,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f'--user-data-dir={temp_user_data_dir}')
 chrome_options.add_argument(f'--disk-cache-dir={temp_cache_dir}')
 chrome_options.binary_location = "/usr/bin/chromium-browser"
-#chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
 chrome_options.add_argument('--disable-software-rasterizer')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--start-maximized') 
