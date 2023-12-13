e5c88fa36a8359e026f8742c56fd2417619d5974
nhaun24 <61599705+nhaun24@users.noreply.github.com>
2023-12-13 13:58:55 -0500

    Update Kiosk.py

--- Kiosk.py
+++ Kiosk.py
@@ -48,12 +48,15 @@ chrome_options.binary_location = "/usr/bin/chromium-browser"
 #chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-software-rasterizer')
-chrome_options.add_argument('--disable-extensions')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--start-maximized') 
 chrome_options.add_argument('--disable-infobars')
 chrome_options.add_argument('--disable-popup-blocking')
-chrome_options.add_argument('--start-maximized') 
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-notifications')
+chrome_options.add_argument('--disable-save-password-bubble')
+chrome_options.add_argument('--disable-translate')
 
 # Read the credentials from the configuration file
 config = configparser.ConfigParser()
