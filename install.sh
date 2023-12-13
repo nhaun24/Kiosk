9a37d418e59c545eb8f8a4372fc49b7c95cb65a8
nhaun24 <61599705+nhaun24@users.noreply.github.com>
2023-12-13 15:29:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# Install WebDriver
+sudo apt-get install chromium-chromedriver -y
+
+# Install Python 3
+sudo apt-get install python3 -y
+
+# Install QZ-tray
+wget -qO - qz.sh | bash
+
+# Install xvfb
+sudo apt-get install xvfb -y
+# Install Python dependencies using pip
+sudo apt-get install python3-pip -y
+
+# Install required Python packages
+pip3 install configparser selenium
+
+sudo cd /var
+sudo mkdir kiosk
+sudo cd kiosk
+sudo git init
+sudo 
+
+echo "Installed successfully."
 
