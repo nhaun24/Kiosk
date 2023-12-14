b756743d2909d77a6f214b599440ca0e89ac31c4
nhaun24 <61599705+nhaun24@users.noreply.github.com>
2023-12-14 13:18:53 -0500

    Update Kiosk2.py

--- Kiosk2.py
+++ Kiosk2.py
@@ -54,7 +54,7 @@ driver_path = '/bin/chromium-browser'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f'--user-data-dir={temp_user_data_dir}')
 chrome_options.add_argument(f'--disk-cache-dir={temp_cache_dir}')
-chrome_options.binary_location = "/usr/bin/chromium-browser"
+#chrome_options.binary_location = "/usr/bin/chromium-browser"
 chrome_options.add_argument('--disable-software-rasterizer')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--start-maximized') 
