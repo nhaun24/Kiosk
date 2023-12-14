42a908b46cf9bda424f07de39a885d08e2d5c5c7
nhaun24 <61599705+nhaun24@users.noreply.github.com>
2023-12-14 17:32:47 -0500

    Update Update.py

--- Update.py
+++ Update.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Change directory to the specified path
-os.chdir(r"C:\Users\Admin\Documents\Git")
+os.chdir(r"/var/kiosk")
 
 # Execute the Git pull command
 os.system('git pull --no-verify https://github.com/nhaun24/Kiosk main')
