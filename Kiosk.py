28234bbb96d202cafc3e471a9215d6efbba34b80
nhaun24 <61599705+nhaun24@users.noreply.github.com>
2023-12-19 15:39:53 -0500

    Update Kiosk.py

--- Kiosk.py
+++ Kiosk.py
@@ -34,7 +34,7 @@ driver_path = '/var/kiosk/conf'
 
 # Create Edge options
 edge_options = Options()
-edge_options.use_chromium = True
+#edge_options.use_chromium = True
 edge_options.add_argument("--kiosk")  # Add this argument to start the browser in full-screen mode
 
 # Read the credentials from the configuration file
